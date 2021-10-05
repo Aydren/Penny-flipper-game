d9f1b7ea8e12387a8f83573a66101ca9168b127a
Adrian Reyes <adrian_reyes1@yahoo.com>
2021-10-04 19:04:22 -0700

    Starting penny flip game repo

---
+++ index.js
@@ -0,0 +1,9 @@
+/* 
+1. create a funcition that returns a random # for a coin flip. like 0 or 1.
+2. create an event lisetener that acts as a flip the coin function that fires the random number function.
+3. create a if statement that accounts for the returned value of 0 or 1
+        - score table updates to display the coin flipper results
+        - increase counter for coin
+        - provides a percentage of teh results
+        - updates the photo of the coin after flip
+ */
